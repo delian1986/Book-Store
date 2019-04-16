6a93704ee1464fedc773053ff01580740c9650a8
delian1986 <delian1914@gmail.com>
2019-04-16 21:07:19 +0300

    client remake

--- client/src/app/components/shared/index.ts
+++
@@ -1,7 +0,0 @@
-import { NavbarComponent } from './navbar/navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-
-export const sharedComponents=[
-    NavbarComponent,
-    FooterComponent
-]
