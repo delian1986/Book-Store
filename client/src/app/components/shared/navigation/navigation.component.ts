4f321db0265af32d5774e1c43709c9f4152d3c17
delian1986 <delian1914@gmail.com>
2019-04-19 00:11:24 +0300

    state

--- client/src/app/components/shared/navigation/navigation.component.ts
+++ client/src/app/components/shared/navigation/navigation.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { State, select } from '@ngrx/store';
+import { AppState } from 'src/app/core/store/app.state';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-navigation',
@@ -6,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
+  email: string;
 
-  
-  constructor() { }
+  constructor(
+    public authService: AuthService,
+    private store: State<AppState>
+  ) {
+    debugger
+    this.email=this.authService.email;
+
+  }
 
   ngOnInit() {
   }
